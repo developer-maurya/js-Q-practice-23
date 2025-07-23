7e260e5aea515d7dc697c3f414018edf0c36e5fc
Chitranjan kumar <chitranjan75075@gmail.com>
2025-07-23 23:42:01 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,71 @@
+let score ="30"
+console.log(typeof score)
+console.log(typeof score)
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)  
+
+// "33"  => 33
+// "33abc" => NaN
+// true => 1  /  false => 0
+
+let nameCon = "chitranjan"
+let nameConOne = Boolean(nameCon)
+
+console.log(nameConOne)
+
+// 1 => True  / 0=> false
+//" " => false
+// "chitranjan" => true
+
+let someNumber = 33;
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber);
+
+
+// ------------------Opertertion--------------------
+let num = 3
+let negNum = -num
+console.log(negNum)
+
+
+let str1 = "chitranjan"
+let str2 =" kumar"
+
+let str3 = "str1" + "str2"
+console.log(str3)     
+// chitranjan kumar
+
+console.log("1" + 2);
+// 12
+console.log(1 + "2");
+// 12
+console.log("1" + 2 + 2 );
+// 122
+console.log(1 + 2 + "2" );
+// 32
+
+
+console.log("2" > 1)
+console.log("02" > 1);
+
+console.log(null > 0);
+// false
+console.log(null == 0)
+// false
+console.log(null >= 0);
+// true
+
+console.log(undefined > 0);
+// false
+console.log(undefined == 0)
+// false
+console.log(undefined < 0);
+// false
+
+
+
+
+
+
